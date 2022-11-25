667ca882a48977a1ce1f6e8d7aa05e0f8710d588
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-25 17:21:41 +0700

    repair method void main

--- Exercise3_148/Program.cs
+++ Exercise3_148/Program.cs
@@ -117,12 +117,14 @@ namespace Exercise3_148
             {
                 try
                 {
-                    Console.WriteLine("\nMenu ");
-                    Console.WriteLine("1. View All records in the list");
-                    Console.WriteLine("2. Search for a second on the list");
-                    Console.WriteLine("3. Display the first record in the list");
-                    Console.WriteLine("4. Exit");
-                    Console.WriteLine("Enter your choice (1-4): ");
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. Display the first record in the list");
+                    Console.WriteLine("6. Exit\n");
+                    Console.WriteLine("Enter your choice (1-6): ");
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
                     {
