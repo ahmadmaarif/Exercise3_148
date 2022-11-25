15bb769b51d8d8cd535c01cc948e38e5753c5825
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-11-25 16:10:11 +0700

    Add project files.

---
+++ Exercise3_148/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_148
+{
+    class Node
+    {
+        /*create Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+}
